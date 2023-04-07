b44e64e660b83ea7b83dc90d59d69eac7c526619
leen8908 <eanli2000@gmail.com>
2023-04-07 11:43:12 +0800

    update auth.py

--- app/routers/api_v1/auth.py
+++ app/routers/api_v1/auth.py
@@ -27,22 +27,6 @@ from fastapi.encoders import jsonable_encoder
 router = APIRouter()
 
 
-# # OAuth settings
-# GOOGLE_CLIENT_ID = settings.GOOGLE_CLIENT_ID or None
-# GOOGLE_CLIENT_SECRET = settings.GOOGLE_CLIENT_SECRET or None
-# if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-#     raise HTTPException(501, 'Missing env variables')
-
-# # Set up oauth
-# config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
-# starlette_config = Config(environ=config_data)
-# oauth = OAuth(starlette_config)
-# oauth.register(
-#     name='google',
-#     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-#     client_kwargs={'scope': 'openid email profile'},
-# )
-
 @router.post('/sso-login')
 def google_auth(request:Request, response: Response, db: Session =Depends(deps.get_db), credential: str = Form(...)) -> Any:
     """
@@ -112,45 +96,3 @@ def google_auth(request:Request, response: Response, db: Session =Depends(deps.g
             status_code=401,
             content={"message": "Unauthorized", "data": None},
         )
-    # data = parseJwt(response.credential)
-    # request.session['user'] = dict(user_data)
-    # # 檢查此google帳號是否已建立帳號
-    # user = crud.user.get_by_email(Depends(deps.get_db), email=dict(user_data).email)
-
-    # #帳號已建立，回傳 matchingRoom
-    # if user:
-    #     return
-    # #帳號尚未建立，create user
-    # else:
-
-# GOOGLE_SECRET_KEY = settings.GOOGLE_SECRET_KEY or None
-# if GOOGLE_SECRET_KEY is None:
-#     raise 'Missing SECRET_KEY'
-# auth_app.add_middleware(SessionMiddleware, secret_key=GOOGLE_SECRET_KEY)
-
-# @auth_app.post('/sso-login')
-# async def login(request: Request):
-#     redirect_uri = request.url_for('auth-token')  # This creates the url for the /google-auth endpoint
-#     return await oauth.google.authorize_redirect(request, redirect_uri)
-
-
-# @auth_app.post('/auth-token')
-# async def auth(request: Request):
-#     try:
-#         access_token = await oauth.google.authorize_access_token(request)
-#     except OAuthError:
-#         return HTTPException(
-#             status_code=401,
-#             detail='Could not validate credentials',
-#             headers={'WWW-Authenticate': 'Bearer'},
-#         )
-#     user_data = await oauth.google.parse_id_token(request, access_token)
-#     request.session['user'] = dict(user_data)
-#     # 檢查此google帳號是否已建立帳號
-#     user = crud.user.get_by_email(Depends(deps.get_db), email=dict(user_data).email)
-
-#     #帳號已建立，回傳 matchingRoom
-#     if user:
-#         return
-#     #帳號尚未建立，create user
-#     else:
